8489b9f58bd16d3d20978e2a1594fbfffc518313
Иванов Кирилл Андреевич <ivanov.k@ez.local>
2019-08-15 15:56:23 +0700

    Добавил еще один выводимый знак в текущие показания тенозодатчика.

--- src/klever_src/lcd_menu/menu_init.c
+++ src/klever_src/lcd_menu/menu_init.c
@@ -49,7 +49,7 @@ void menuInit(void){
 			initItemParameter(&(item[item_index++]),"a лев.   ", W(FORMULAS), initObjChangeable(&(obj[obj_index++]), inc_angle_left, dec_angle_left, show_angle_left), 1, 2, line);
 			initItemParameter(&(item[item_index++]),"a прав.  ", W(FORMULAS), initObjChangeable(&(obj[obj_index++]), inc_angle_right, dec_angle_right, show_angle_right), 1, 3, line);
 			initItemSubmenu(&(item[item_index++]), "<назад", W(FORMULAS), W(MODE), 0, 7);
-			initItemParameter(&(item[item_index++]), "датч.", W(FORMULAS), initObjNotchangeable(&(obj[obj_index++]), show_cur_sensor), 11, 4, column);
+			initItemParameter(&(item[item_index++]), "датч.", W(FORMULAS), initObjNotchangeable(&(obj[obj_index++]), show_cur_sensor), 10, 4, column);
 			initItemParameter(&(item[item_index++]), "натяг", W(FORMULAS), initObjNotchangeable(&(obj[obj_index++]), show_cur_tension), 11, 6, column);
 
 		initItemParameter(&(item[item_index++]), 	"Не регул  ", W(MODE), initObjChangeable(&(obj[obj_index++]), switch_mode_not_reg_state, switch_mode_not_reg_state, show_mode_not_reg_state), 1, 2, line);  
